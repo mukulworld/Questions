c89d09525163f5ab12e178acc0c67ed9b87792a2
A. Mukul <60055486+mukulworld@users.noreply.github.com>
2022-11-15 22:26:01 +0530

    Create Leetcode Problem 1544. Make The String Great.cpp

---
+++ C++/Leetcode Problem 1544. Make The String Great.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/make-the-string-great/
+
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char>temp;        
+        
+        for(int i=0;i<s.length();i++){
+            if(!temp.empty() && abs(s[i] - temp.top()) == 32)
+            temp.pop();
+           else  temp.push(s[i]);                  
+        }
+                
+        string ans= "";
+        
+        while(!temp.empty()){
+            ans = temp.top() + ans;
+            temp.pop();                         
+        }
+        
+        return ans;
+    }
+};
