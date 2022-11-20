69e11a4de015642f5686c3d22bff5de4ab7e944f
A. Mukul <60055486+mukulworld@users.noreply.github.com>
2022-11-20 16:03:11 +0530

    Update Leetcode Problem 162. Find Peak Element.cpp

--- C++/Leetcode Problem 162. Find Peak Element.cpp
+++ C++/Leetcode Problem 162. Find Peak Element.cpp
@@ -19,3 +19,18 @@ public:
 
 // Binary Search O(logn)
 
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+                
+        int l=0,r=nums.size()-1,mid;
+        
+        while(l<r){
+            mid = (r-l)/2 + l;
+            if(nums[mid]  > nums[mid +1]) r = mid;
+            else l = mid+1;            
+        }
+        
+        return l;
+    }
+};
