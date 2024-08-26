fe31633e5258f4a17055d434ee1cea9f63eccf58
A. Mukul <60055486+mukulworld@users.noreply.github.com>
2024-08-26 17:48:18 +0530

    Update Leetcode problem 50. Pow(x, n).cpp
    Optimized code

--- SDE Striver/Array /Leetcode problem 50. Pow(x, n).cpp
+++ SDE Striver/Array /Leetcode problem 50. Pow(x, n).cpp
@@ -3,14 +3,13 @@ public:
     double myPow(double x, int n) {
         double ans = 1;
         long long s=n;
-        if(n<0) s = -1*n;
-
-        for(long i=0;i<s;i++){
-            ans = ans*x;
+        if(n<0) s = -s;
+        
+        while(s>0){
+            if(s%2==1)ans = ans*x,s-=1;
+            else x = x*x,s/=2;
         }
-    
-    if(n>0) return ans;
-    return 1/ans;
-
+        if(n<0) return 1.0/ans;
+        return ans;
     }
 };
