821b27b0e0be37215cd9c799cf0a087111c72a21
A. Mukul <60055486+mukulworld@users.noreply.github.com>
2024-08-30 22:58:20 +0530

    Create Leetcode problem 53. Maximum Subarray.cpp

---
+++ SDE Striver/Array /Leetcode problem 53. Maximum Subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0,maxm = INT_MIN;
+
+        for(int i=0; i< nums.size(); i++){
+            sum += nums[i];
+            maxm = max(maxm, sum);
+            if(sum <= 0) sum =0;
+         
+            
+        }
+        return maxm;
+    }
+};
